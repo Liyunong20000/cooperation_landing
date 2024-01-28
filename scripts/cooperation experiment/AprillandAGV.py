e6d921e1e2452c142383775932ff0e4defac40a7
Yunong_LI <li@hnl.t.u-tokyo.ac.jp>
2024-01-28 21:27:29 +0900

    [myagv][scripts] figure and main scripts
    1. Creat 0.032m apriltag
    2. change the logic of apriltag follow. I try to put the command publishing work in the callback function.

--- scripts/cooperation experiment/AprillandAGV.py
+++ scripts/cooperation experiment/AprillandAGV.py
@@ -9,7 +9,8 @@ from std_msgs.msg import Empty, UInt8
 from aerial_robot_msgs.msg import FlightNav
 from apriltag_ros.msg import AprilTagDetectionArray
 from nav_msgs.msg import Odometry
-from geometry_msgs.msg import TransformStamped
+from geometry_msgs.msg import Twist
+from sensor_msgs.msg import CameraInfo
 
 
 # It is for  the Coopration for Mini_Quadrotor and MyAGV
@@ -22,37 +23,54 @@ class AprillandagvNode:
         print(f'Hi, I am Cloud Cube')
         rospy.init_node('Aprillandagv', anonymous=True)
 
-        self.agv_x, self.agv_y, self.agv_z = 0.0, 0.0, 0.0
         self.drone_x, self.drone_y, self.drone_z = 0.0, 0.0, 0.0
+        self.takeoff_x, self.takeoff_y, self.takeoff_z = 0.0, 0.0, 0.0
+        self.agv_x, self.agv_y, self.agv_z = 0.0, 0.0, 0.0
+        self.lvol_x, self.lvol_y, self.lvol_z = 0.0, 0.0, 0.0
+        self.anglevol_x, self.anglevol_y, self.anglevol_z = 0.0, 0.0, 0.0
         self.april_x, self.april_y, self.april_z = 0.0, 0.0, 0.0
-        self.takeoff_x, self.takeoff_y, self.takeoff_Tz = 0.0, 0.0, 0.0
-       
+        self.D = 0
+
+        self.time_rece = rospy.Time()
         self._seq = 0
         self.state = 0
+        self.beginland = 1
+        self.beginfollow = 0
 
         # Subscribe and publish.
         rospy.Subscriber('/tag_detections', AprilTagDetectionArray, self._callback_apriltag)
         rospy.Subscriber('/quadrotor/uav/cog/odom', Odometry, self._callback_position)
         rospy.Subscriber('/quadrotor/flight_state', UInt8, self._callback_state)
-        self.pub_nav = rospy.Publisher('/quadrotor/uav/nav', FlightNav, queue_size=10)
+        self.pub_drone_nav = rospy.Publisher('/quadrotor/uav/nav', FlightNav, queue_size=10)
         self.pub_takeoff = rospy.Publisher('/quadrotor/teleop_command/takeoff', Empty, queue_size=10)
         self.pub_land = rospy.Publisher('/quadrotor/teleop_command/land', Empty, queue_size=10)
-        # !!!订阅AGV位置的话题
-        # !!!发布AGV移动的话题
 
-        self.tf_buffer = tf2_ros.Buffer()
-        # Initialize a TransformListener
-        self.tf_listener = tf2_ros.TransformListener(self.tf_buffer)
+        self.pub_agv_nav = rospy.Publisher('/cmd_vel', Twist, queue_size=10, tcp_nodelay=True)
+
+        rospy.set_param('/move_parameter', 0.7)
+        self.move_parameter = rospy.get_param("/move_parameter")
+        rospy.set_param('/converge_interval', 0.05)
+        self.converge_interval = rospy.get_param("/converge_interval")
+        rospy.set_param('/above_z', 0.2)
+        self.above_z = rospy.get_param("/above_z")
 
     def _callback_apriltag(self, data):
-        # get the apriltag`s position information compare with camera coordination
-        # use transform to gain the
-        transform = self.tf_buffer.lookup_transform('world', 'Target', rospy.Time.now(),rospy.Duration(0.1))
-        self.april_x = transform.transform.translation.x
-        self.april_y = transform.transform.translation.y
-        self.april_z = transform.transform.translation.z
+        if data.detections:
+            # rospy.loginfo("????????????????????????????????detect tag!")
+            a = data.detections[0]
+            self.april_x = a.pose.pose.pose.position.x
+            self.april_y = a.pose.pose.pose.position.y
+            # self.D = 1
+            if self.beginfollow == 1:
+                self.lvol_x = self.move_parameter * self.april_y
+                self.lvol_y = - self.move_parameter * self.april_x
+                if abs(self.lvol_x) < 0.5 and abs(self.lvol_y) < 0.5:
+                    self.agv_nav_info(self.lvol_x, self.lvol_y, 0)
+        else:
+            self.D = 0
+            if self.beginfollow == 1:
+                self.agv_nav_info(0, 0, 0)
 
-    
     # Get the position information of the drone
     def _callback_position(self, odom_msg):  
         self.drone_x = odom_msg.pose.pose.position.x
@@ -64,20 +82,25 @@ class AprillandagvNode:
         self.state = msg.data
 
     # drone takeoff
-    def takeoff(self):  
+    def takeoff(self):
         time.sleep(0.5)
         rospy.loginfo("Publishing takeoff command...")
         empty_msg = Empty()
         self.pub_takeoff.publish(empty_msg)
     
     # drone land
-    def land(self):  
+    def land(self):
         time.sleep(0.5)
         rospy.loginfo("Publishing land command...")
         empty_msg = Empty()
         self.pub_land.publish(empty_msg)
 
-    
+    def converge(self, x, y, z):
+        while not rospy.is_shutdown():
+            if abs(self.drone_x - x) < self.converge_interval and abs(self.drone_y - y) < self.converge_interval and abs(self.drone_z - z) < self.converge_interval:  # reach hover state
+                break
+            time.sleep(0.1)
+
     # record the take off position
     def record_takeoff_position(self):
         self.takeoff_x = self.drone_x
@@ -85,7 +108,15 @@ class AprillandagvNode:
         self.takeoff_z = self.drone_z
         print(self.takeoff_x, self.takeoff_y)
 
-    def nav_info(self, x, y, z):
+    def agv_nav_info(self, lx, ly, az):
+        agv_nav_msg = Twist()
+        agv_nav_msg.linear.x = lx
+        agv_nav_msg.linear.y = ly
+        agv_nav_msg.angular.z = az
+
+        self.pub_agv_nav.publish(agv_nav_msg)
+
+    def drone_nav_info(self, x, y, z):
         flight_nav_msg = FlightNav()
         flight_nav_msg.header.seq = self._seq
         self._seq += 1
@@ -109,57 +140,47 @@ class AprillandagvNode:
         flight_nav_msg.target_vel_z = 0.0
         flight_nav_msg.target_pos_diff_z = 0.0
 
-        self.pub_nav.publish(flight_nav_msg)
+        self.pub_drone_nav.publish(flight_nav_msg)
 
+    # def follow_camera(self, ctl_rate: int):
+    #     rate = rospy.Rate(ctl_rate)
+    #     while not rospy.is_shutdown():
+    #         if self.D:
+    #             self.lvol_x = self.move_parameter * self.april_y
+    #             self.lvol_y = - self.move_parameter * self.april_x
+    #             if abs(self.lvol_x) < 0.5 and abs(self.lvol_y) < 0.5:
+    #                 self.agv_nav_info(self.lvol_x, self.lvol_y, 0)
+    #
+    #             #     if (rospy.Time.now() - self.time_rece).to_sec() > 0.5:
+    #             #         self.D = 0
+    #
+    #         else:
+    #             self.agv_nav_info(0, 0, 0)
+    #
+    #         rate.sleep()
 
     def land_camera(self):
 
         # self.get_user_input()
         while not rospy.is_shutdown():
-            if self.state == 5: # reach hover state
+            if self.state == 5:
                 break
             time.sleep(0.1)
-
-        tz = self.Tz + 0.3
-        self.nav_info(self.Tx, self.Ty, tz)
-        print(f'Move to higher Z')
-        while not rospy.is_shutdown():
-            if self.Pz - tz < 0.03:
-                break
-            time.sleep(0.1)
-        print(f'Wait for 3 seconds!')
-        time.sleep(3)
-
-        while not rospy.is_shutdown():
-            rx = self.Rx
-            ry = self.Ry
-            if rx != 0 and ry != 0:
-                self.nav_info(rx, ry, tz)
-                while not rospy.is_shutdown():
-                    if (self.Px - rx) < 0.03 and (self.Py - ry) < 0.03:
-                        break
-                    time.sleep(0.1)
-                break
-            #break
-        print(f'the apriltags position is rx ={rx}, ry = {ry}')
-        while not rospy.is_shutdown():
-            if math.sqrt((self.Px - rx) ** 2 + (self.Py - ry) ** 2) < 0.01:
-                print(f'Safe landing!')
-                print(f'Before landing on X = {self.Px}, Y = {self.Py}')
-                self.land()
-                time.sleep(1)
-                print(f'After landing on X = {self.Px}, Y = {self.Py}')
-                sys.exit()
-
+        tz = self.takeoff_z + self.above_z
+        self.drone_nav_info(self.takeoff_x, self.takeoff_y, tz)
+        self.beginfollow = 1
+        print(f'Move to above takeoff_Z')
+        self.converge(self.takeoff_x, self.takeoff_y, tz)
+        if abs(self.april_x) < 0.005 and abs(self.april_y) < 0.005 and self.beginland == 1:
+            self.land()
+            self.beginland = 0
 
 
 if __name__ == '__main__':
     node = AprillandagvNode()
     time.sleep(3)
-    # node.publish_flight_nav()
     node.record_takeoff_position()
     node.takeoff()
     node.land_camera()
-
     while not rospy.is_shutdown():
         rospy.spin()
