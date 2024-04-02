7117a0dbdfdd707b5d79db459b53e1e1e29fee92
Yunong_LI <li@hnl.t.u-tokyo.ac.jp>
2024-04-02 21:34:45 +0900

    [myagv][code] Add the orientation information
    successful.

--- scripts/cooperation experiment/Aprilfollow_demo.py
+++ scripts/cooperation experiment/Aprilfollow_demo.py
@@ -1,13 +1,9 @@
 #! /usr/bin/env python
 # -*- coding: utf-8 -*-
 import rospy, sys
-import time
-import math
-import tf2_ros
-import tf
-from std_msgs.msg import Empty, UInt8
-from aerial_robot_msgs.msg import FlightNav
+import numpy as np
 from apriltag_ros.msg import AprilTagDetectionArray
+import math
 from nav_msgs.msg import Odometry
 from geometry_msgs.msg import TransformStamped
 from geometry_msgs.msg import Twist
@@ -56,9 +52,17 @@ class AprilfollowNode:
             a = data.detections[0]
             self.april_x = a.pose.pose.pose.position.x
             self.april_y = a.pose.pose.pose.position.y
+            quaternion_x = a.pose.pose.pose.orientation.x
+            quaternion_y = a.pose.pose.pose.orientation.y
+            quaternion_z = a.pose.pose.pose.orientation.z
+            quaternion_w = a.pose.pose.pose.orientation.w
+
+            self.april_z = self.quaternion_to_euler_angle(quaternion_x,quaternion_y,quaternion_z,quaternion_w)
+            print("euler_z (degree):", self.april_z)
             self.D = 1
             self.lvol_x = self.move_parameter * self.april_y
             self.lvol_y = - self.move_parameter * self.april_x
+            self.anglevol_z = 0.05* self.move_parameter * self.april_z
             apriltag_time = rospy.Time.now()
 
             #print(f'apriltag_time:{apriltag_time.to_sec()}')
@@ -66,7 +70,7 @@ class AprilfollowNode:
                 navigation_time = rospy.Time.now()
 
                 #print(f'navigation_time:{navigation_time.to_sec()}')
-                self.agv_nav_info(self.lvol_x, self.lvol_y, 0)
+                self.agv_nav_info(self.lvol_x, self.lvol_y, self.anglevol_z)
         else:
             self.D = 0
             self.agv_nav_info(0, 0, 0)
@@ -94,6 +98,20 @@ class AprilfollowNode:
 
             print(f'x = {self.april_x},y = {self.april_y}')
             rate.sleep()
+
+    import numpy as np
+
+    def quaternion_to_euler_angle(self, x, y, z, w):
+
+        R = np.array([[1 - 2 * y ** 2 - 2 * z ** 2, 2 * x * y - 2 * w * z, 2 * x * z + 2 * w * y],
+                      [2 * x * y + 2 * w * z, 1 - 2 * x ** 2 - 2 * z ** 2, 2 * y * z - 2 * w * x],
+                      [2 * x * z - 2 * w * y, 2 * y * z + 2 * w * x, 1 - 2 * x ** 2 - 2 * y ** 2]])
+        # theta_x = math.degrees(np.arctan2(R[2, 1], R[2, 2]))
+        # theta_y = math.degrees(np.arctan2(-R[2, 0], np.sqrt(R[2, 1] ** 2 + R[2, 2] ** 2)))
+        theta_z = math.degrees(np.arctan2(R[1, 0], R[0, 0]))
+        return theta_z
+
+    # 示例四元数
 
 
 
