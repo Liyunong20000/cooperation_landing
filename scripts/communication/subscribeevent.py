e5fc5d81f5f974645db95380e1e4514708fc9384
Liyunong20000 <li@hnl.t.u-tokyo.ac.jp>
2024-10-30 12:07:07 +0900

    [qilin][msg,launch] change drone nav topic

--- scripts/communication/subscribeevent.py
+++ scripts/communication/subscribeevent.py
@@ -10,7 +10,7 @@ from pandas import set_eng_float_format
 from std_msgs.msg import Empty, UInt8
 from nav_msgs.msg import Odometry
 from std_srvs.srv import Trigger
-from geometry_msgs.msg import Twist
+from geometry_msgs.msg import PoseStamped
 from geometry_msgs.msg import Pose
 
 # It is for  the Coopration for Mini_Quadrotor and Qilin
@@ -26,28 +26,43 @@ class SubscribeeventNode:
 
         self._seq = 0
         # Subscribe and publish.
-        rospy.Subscriber('/quadrotor/uav/nav/trigger', Empty, self._callback_nav_trigger)
-        rospy.Subscriber('/quadrotor/uav/nav/info', FlightNav, self._callback_nav_info)
+        rospy.Subscriber('/quadrotor/target_pose/info', PoseStamped, self._callback_target_pose_info)
+        rospy.Subscriber('/quadrotor/target_pose/trigger', Empty, self._callback_target_pose_trigger)
+        # rospy.Subscriber('/quadrotor/uav/nav/info', FlightNav, self._callback_nav_info)
 
 
         # self.pub_event = rospy.Publisher('/uavandgr/event', UInt8, queue_size=10)
-
-        self.pub_drone_nav = rospy.Publisher('/quadrotor/uav/nav', FlightNav, queue_size=10)
+        self.pub_drone_target = rospy.Publisher('/quadrotor/target_pose', PoseStamped, queue_size=10)
+        # self.pub_drone_nav = rospy.Publisher('/quadrotor/uav/nav', FlightNav, queue_size=10)
         self.pub_takeoff = rospy.Publisher('/quadrotor/teleop_command/takeoff', Empty, queue_size=10)
         self.pub_land = rospy.Publisher('/quadrotor/teleop_command/land', Empty, queue_size=10)
 
         self.x_y_mode, self.z_mode = 0, 0
         self.target_x, self.target_y, self.target_z= 0.0, 0.0, 0.0
+        self.target_ox, self.target_oy, self.target_oz, self.target_ow = 0.0, 0.0, 0.0, 0.0
         self.yaw_nav_mode, self.target_omega_z, self.target_yaw = 0, 0.0, 0.0
-    def _callback_nav_info(self, msg):
-        self.x_y_mode= msg.pos_xy_nav_mode
-        self.target_x= msg.target_pos_x
-        self.target_y= msg.target_pos_y
-        self.z_mode= msg.pos_z_nav_mode
-        self.target_z= msg.target_pos_z
-        self.yaw_nav_mode = msg.yaw_nav_mode
-        self.target_omega_z = msg.target_omega_z
-        self.target_yaw = msg.target_yaw
+    # def _callback_nav_info(self, msg):
+    #     self.x_y_mode= msg.pos_xy_nav_mode
+    #     self.target_x= msg.target_pos_x
+    #     self.target_y= msg.target_pos_y
+    #     self.z_mode= msg.pos_z_nav_mode
+    #     self.target_z= msg.target_pos_z
+    #     self.yaw_nav_mode = msg.yaw_nav_mode
+    #     self.target_omega_z = msg.target_omega_z
+    #     self.target_yaw = msg.target_yaw
+
+    def _callback_target_pose_info(self, msg):
+        self.target_x = msg.pose.position.x
+        self.target_y = msg.pose.position.y
+        self.target_z = msg.pose.position.z
+        self.target_ox = msg.pose.orientation.x
+        self.target_oy = msg.pose.orientation.y
+        self.target_oz = msg.pose.orientation.z
+        self.target_ow = msg.pose.orientation.w
+
+    def _callback_target_pose_trigger(self):
+        self.drone_target_pose(self.target_x, self.target_y, self.target_z, self.target_ox, self.target_oy, self.target_oz, self.target_ow)
+        print(f"pub target pose")
 
     def _callback_nav_trigger(self, msg):
         self.drone_nav_info(self.x_y_mode, self.target_x, self.target_y, self.z_mode, self.target_z,self.yaw_nav_mode, self.target_omega_z, self.target_yaw)
@@ -92,6 +107,18 @@ class SubscribeeventNode:
 
         self.pub_drone_nav.publish(flight_nav_msg)
 
+    def drone_target_pose(self,x,y,z,ox,oy,oz,ow):
+        drone_target_pose = PoseStamped()
+        drone_target_pose.header.frame_id = 'world'
+        drone_target_pose.pose.position.x = x
+        drone_target_pose.pose.position.y = y
+        drone_target_pose.pose.position.z = z
+        drone_target_pose.pose.orientation.x = ox
+        drone_target_pose.pose.orientation.y = oy
+        drone_target_pose.pose.orientation.z = oz
+        drone_target_pose.pose.orientation.w = ow
+
+        self.pub_drone_target.publish(drone_target_pose)
 
 
 
